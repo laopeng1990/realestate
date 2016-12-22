5a2e64aaba34201fb17adc4e715f1ff88846350b
pengfeiwen <wenpengfei@tigerbrokers.com>
2016-12-22 19:35:26 +0800

    MOD: add statistics task

--- src/main/java/com/wpf/realestate/task/LjStatisticsTask.java
+++ src/main/java/com/wpf/realestate/task/LjStatisticsTask.java
@@ -3,10 +3,12 @@ package com.wpf.realestate.task;
 import com.wpf.realestate.extractor.LjExtractor;
 import com.wpf.realestate.storage.HouseRedisDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by wenpengfei on 2016/12/22.
  */
+@Service
 public class LjStatisticsTask extends BaseTask {
 
     private LjExtractor ljExtractor;
